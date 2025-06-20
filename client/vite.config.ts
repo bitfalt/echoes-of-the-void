b9e858d79c835d0ecd0ce926b6a574135bb6a33b
Daniel Garbanzo <75431447+bitfalt@users.noreply.github.com>
2025-06-20 11:09:05 -0600

    Initialize feat/integrate-training-method

---
+++ client/vite.config.ts
@@ -0,0 +1,27 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
+import fs from "fs";
+
+export default defineConfig({
+  plugins: [react(), wasm(), topLevelAwait()],
+  server: {
+    port: 3002,
+    https: {
+      key: fs.readFileSync("mkcert+1-key.pem"),
+      cert: fs.readFileSync("mkcert+1.pem"),
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+    },
+  },
+  optimizeDeps: {
+    include: ['buffer'],
+  },
+});
